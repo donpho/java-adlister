c55c989c97d931d2d95808d6a3741c049dddda1a
Don Pho <donpho@Dons-MacBook-Air.local>
2018-08-22 10:39:53 -0500

    mvc exercise

--- src/main/java/AdsServlet.java
+++ src/main/java/AdsServlet.java
@@ -1,10 +1,21 @@
-@javax.servlet.annotation.WebServlet(name = "AdsServlet")
-public class AdsServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet(name = "AdsServlet", urlPatterns = "/ads")
+public class AdsServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, java.io.IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Ads adsDao = DaoFactory.getAdsDao();
+        List<Ad> ads = adsDao.all();
+        request.setAttribute("ads",ads);
+        request.getRequestDispatcher("/ads/index.jsp").forward(request,response);
     }
 }
--- src/main/webapp/ads/index.jsp
+++ src/main/webapp/ads/index.jsp
@@ -1,3 +1,4 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%--
   Created by IntelliJ IDEA.
   User: donpho
@@ -8,9 +9,15 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>$Title$</title>
+    <title>Ads</title>
 </head>
 <body>
-$END$
+    <h1>All of the ads</h1>
+    <c:forEach var="ad" items="${ads}">
+        <div>
+            <h2>${ad.title}</h2>
+            <p>${ad.description}</p>
+        </div>
+    </c:forEach>
 </body>
 </html>
